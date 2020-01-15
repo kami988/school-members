cf5c3ab3af0bb8a6fcd6f980bbeaa6673aad5b8a
y.kamimoto <y.kamimoto@corp.acall.jp>
2020-01-15 15:34:10 +0900

    add no-autogen-policy to config.json

--- chalicelib/db.py
+++ chalicelib/db.py
@@ -43,7 +43,8 @@ class DynamoDBTodo(TodoDB):
                 'id':  id,
             }
         )
-        return id
+        response = 'create id:' + id + ', subject:' + subject + ', name:' + name + ', remarks:' + remarks + '\n'
+        return response
 
     def update_item(self, id, subject=None, name=None, mail=None, remarks=None):
         # We could also use update_item() with an UpdateExpression.
