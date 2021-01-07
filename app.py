b19e130ebb96022cdbff6d1446820432a53beaf2
kami988 <33030656+kami988@users.noreply.github.com>
2021-01-07 18:24:32 +0900

    feature: cors=True

--- app.py
+++ app.py
@@ -18,22 +18,22 @@ def get_app_db():
     return _DB
 
 #top
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'], cors=True)
 def get_top():
     return 'wlcome to School Members API'
 
 #全員参照
-@app.route('/members', methods=['GET'])
+@app.route('/members', methods=['GET'], cors=True)
 def get_members():
     return get_app_db().list_all_items()
 
 #一人参照
-@app.route('/members/{id}', methods=['GET'])
+@app.route('/members/{id}', methods=['GET'], cors=True)
 def get_member(id):
     return get_app_db().get_item(id)
 
 #追加
-@app.route('/members', methods=['POST'])
+@app.route('/members', methods=['POST'], cors=True)
 def add_new_member():
     body = app.current_request.json_body
     id = get_app_db().add_item(
@@ -45,7 +45,7 @@ def add_new_member():
     return get_member(id)
 
 #更新
-@app.route('/members/{id}', methods=['PUT'])
+@app.route('/members/{id}', methods=['PUT'], cors=True)
 def update_SchoolMembers(id):
     body = app.current_request.json_body
     get_app_db().update_item(
